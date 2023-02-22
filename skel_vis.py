266640dbdf296534f9350870d3ef76a47704faa0
unknown <Mislav.stojanovic@gmail.com>
2023-02-22 04:18:32 +0100

    visualize

--- skel_vis.py
+++ skel_vis.py
@@ -1,33 +1,6 @@
-#import matplotlib
-#matplotlib.use('agg')
-
 import numpy as np
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
-
-plt.style.use('seaborn-poster')
-
-##### konstante
-NUI_SKELETON_POSITION_HIP_CENTER = 0
-NUI_SKELETON_POSITION_SPINE = 1
-NUI_SKELETON_POSITION_SHOULDER_CENTER = 2
-NUI_SKELETON_POSITION_HEAD = 3
-NUI_SKELETON_POSITION_SHOULDER_LEFT = 4
-NUI_SKELETON_POSITION_ELBOW_LEFT = 5
-NUI_SKELETON_POSITION_WRIST_LEFT = 6
-NUI_SKELETON_POSITION_HAND_LEFT = 7
-NUI_SKELETON_POSITION_SHOULDER_RIGHT = 8
-NUI_SKELETON_POSITION_ELBOW_RIGHT = 9
-NUI_SKELETON_POSITION_WRIST_RIGHT = 10
-NUI_SKELETON_POSITION_HAND_RIGHT = 11
-NUI_SKELETON_POSITION_HIP_LEFT = 12
-NUI_SKELETON_POSITION_KNEE_LEFT = 13
-NUI_SKELETON_POSITION_ANKLE_LEFT = 14
-NUI_SKELETON_POSITION_FOOT_LEFT = 15
-NUI_SKELETON_POSITION_HIP_RIGHT = 16
-NUI_SKELETON_POSITION_KNEE_RIGHT = 17
-NUI_SKELETON_POSITION_ANKLE_RIGHT = 18
-NUI_SKELETON_POSITION_FOOT_RIGHT = 19
 
 NUI_SKELETON_POSITION_COUNT = 20
 
@@ -78,32 +51,21 @@ nui_skeleton_conn = np.matrix([
 	[ANKLE_RIGHT, FOOT_RIGHT]
 ])
 
-####kraj konstanta
 
-def skel_vis(X, tidix):
-    
-    assert tidix >= 0
-    assert tidix <= X.shape[0]
-
-    xyz_ti=X[tidix,:]
-
-    skel=xyz_ti.reshape( NUI_SKELETON_POSITION_COUNT, 4)
- 
+def skel_vis(X):
     fig = plt.figure()
-    ax = fig.add_subplot(111,projection='3d')
+    ax = plt.axes(projection='3d')
     ax.set_title('3D Skeleton')
     ax.set_xlabel('x', labelpad=20)
     ax.set_ylabel('y', labelpad=20)
     ax.set_zlabel('z', labelpad=20)
     
-    ax.scatter3D(skel[:,0], skel[:,2], skel[:,1], zdir='z', c= 'red');
+    ax.scatter3D(X[:,0], X[:,2], X[:,1], zdir='z', c= 'red');
 
     for i in range(nui_skeleton_conn.size//2):
         prva = nui_skeleton_conn[i, 0];
         druga = nui_skeleton_conn[i, 1];
     
-        ax.plot([skel[prva , 0], skel[druga , 0]], [skel[ prva, 2], skel[druga, 2 ]],zs=[skel[prva, 1], skel[druga, 1]])
+        ax.plot([X[prva , 0], X[druga , 0]], [X[ prva, 2], X[druga, 2 ]],zs=[X[prva, 1], X[druga, 1]])
     
     plt.show()
-
- 
